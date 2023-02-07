1e8d6242ce0cb1281301a11567460dd5f3772f33
Olena <newsroommy@gmail.com>
2023-02-08 02:45:53 +0300

    add

--- src/modules/Menu/Menu.jsx
+++ src/modules/Menu/Menu.jsx
@@ -1,18 +1,24 @@
-import { Link } from 'react-router-dom';
-import css from './menu-module.css';
+import { NavLink } from 'react-router-dom';
+import css from './menu.module.css';
+
+// перевіряє чи співпадає адреса з тією на яку йде посилання to=''
+const getClassName = ({ isActive }) => {
+  const className = isActive ? `${css.link} ${css.active}` : css.link;
+  return className;
+};
 const Menu = () => {
   return (
     <div className={css.wrapper}>
-      <ul>
+      <ul className={css.menu}>
         <li>
-          <Link className={css.link} to="/">
+          <NavLink className={getClassName} to="/">
             Home
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link className={css.link} to="/movies">
+          <NavLink className={getClassName} to="/movies">
             Movies
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </div>
