f3a65786c0d934c9cec9b6dba9f90279c98affe6
Olena <newsroommy@gmail.com>
2023-02-11 06:10:00 +0300

    add MovieDetailsPage

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -4,28 +4,29 @@ import { getDetailsMovie } from 'shared/styles/services/movie-api';
 
 const MovieDetailsPage = () => {
   const [movie, setMovies] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
   const [genres, setGenres] = useState([]);
   const [date, setDate] = useState('');
   const { movieId } = useParams();
   useEffect(() => {
     const fetchDetailsMovie = async () => {
       try {
-        setLoading(true);
+        // setLoading(true);
         const result = await getDetailsMovie(movieId);
         console.log(result);
         setMovies(result);
         setGenres(result.genres);
         setDate(result.release_date);
       } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
+        // setError(error.message);
       }
+      // finally {
+      //   setLoading(false);
+      // }
     };
     fetchDetailsMovie();
-  }, [setLoading, setMovies, setError]);
+  }, [movieId]);
 
   const elements = genres.map(({ name, id }) => <li key={id}>{name}</li>);
   const year = new Date(date).getFullYear();
