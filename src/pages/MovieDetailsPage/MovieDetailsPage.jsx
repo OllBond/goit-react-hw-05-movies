858823db2d77cbfdd27b3c1a8d2f8d943e7d794c
Olena <newsroommy@gmail.com>
2023-02-11 05:59:37 +0300

    add MovieDetailsPage

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -6,6 +6,8 @@ const MovieDetailsPage = () => {
   const [movie, setMovies] = useState({});
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [genres, setGenres] = useState([]);
+  const [date, setDate] = useState('');
   const { movieId } = useParams();
   useEffect(() => {
     const fetchDetailsMovie = async () => {
@@ -14,6 +16,8 @@ const MovieDetailsPage = () => {
         const result = await getDetailsMovie(movieId);
         console.log(result);
         setMovies(result);
+        setGenres(result.genres);
+        setDate(result.release_date);
       } catch (error) {
         setError(error.message);
       } finally {
@@ -22,17 +26,31 @@ const MovieDetailsPage = () => {
     };
     fetchDetailsMovie();
   }, [setLoading, setMovies, setError]);
+
+  const elements = genres.map(({ name, id }) => <li key={id}>{name}</li>);
+  const year = new Date(date).getFullYear();
+  const { poster_path, original_title, overview, popularity } = movie;
   return (
-    <div>
+    <>
       <button type="button">Go back</button>
-      <img src="" alt="" />
-      <h2>{movie.original_title}</h2>
-      <p>{movie.overview}</p>
-      <p></p>
-      <p></p>
-      <p></p>
-      <p></p>
-    </div>
+      <div>
+        <img
+          src={`https://image.tmdb.org/t/p/w300/${poster_path}`}
+          alt={original_title}
+          loading="lazy"
+        />
+        <div>
+          <h2>
+            {original_title} <span>({year})</span>
+          </h2>
+          <p>{popularity}</p>
+          <h3>Overview</h3>
+          <p>{overview}</p>
+          <h3>Genres</h3>
+          <ul>{elements}</ul>
+        </div>
+      </div>
+    </>
   );
 };
 export default MovieDetailsPage;
