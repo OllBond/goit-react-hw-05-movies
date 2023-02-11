cba43aaa15393a4cc72f518168fc30b4b3974c1c
Olena <newsroommy@gmail.com>
2023-02-11 23:12:26 +0300

    update

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { HiArrowNarrowLeft } from 'react-icons/hi';
 import { getDetailsMovie } from 'shared/styles/services/movie-api';
-
+import css from '../MovieDetailsPage/movieDetailsPage.module.css';
 const MovieDetailsPage = () => {
   const [movie, setMovies] = useState({});
   // const [loading, setLoading] = useState(false);
@@ -24,27 +25,33 @@ const MovieDetailsPage = () => {
     fetchDetailsMovie();
   }, [movieId]);
 
-  const elements = genres.map(({ name, id }) => <li key={id}>{name}</li>);
+  const elements = genres.map(({ name, id }) => (
+    <li key={id} className={css.genresItems}>
+      {name}
+    </li>
+  ));
   const year = new Date(date).getFullYear();
   const { poster_path, original_title, overview, popularity } = movie;
   return (
     <>
-      <button type="button">Go back</button>
-      <div>
+      <button className={css.btnGoBack} type="button">
+        <HiArrowNarrowLeft /> Go back
+      </button>
+      <div className={css.wrapperOneFilm}>
         <img
           src={`https://image.tmdb.org/t/p/w300/${poster_path}`}
           alt={original_title}
           loading="lazy"
         />
-        <div>
+        <div className={css.wrapperDetails}>
           <h2>
             {original_title} <span>({year})</span>
           </h2>
           <p>{popularity}</p>
-          <h3>Overview</h3>
+          <h3 className={css.titleDetails}>Overview</h3>
           <p>{overview}</p>
-          <h3>Genres</h3>
-          <ul>{elements}</ul>
+          <h3 className={css.titleDetails}>Genres</h3>
+          <ul className={css.genresList}>{elements}</ul>
         </div>
       </div>
     </>
