27a5d88c5e982983ad17f6bd3392312b2b68ce72
Olena <newsroommy@gmail.com>
2023-02-11 23:43:35 +0300

    update App.jsx, add UserRoutes.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import { Route, Routes } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Menu from './modules/Menu/Menu';
-import HomePage from './pages/HomePage/HomePage';
-import MoviesPage from './pages/MoviesPage/MoviesPage';
-import MovieDetailsPage from './pages/MovieDetailsPage/MovieDetailsPage';
-import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import UserRoutes from 'UserRoutes';
 import css from './shared/styles/styles.css';
 export const App = () => {
   return (
     <BrowserRouter className={css.styles}>
       <Menu />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<MoviesPage />} />
-        <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <UserRoutes />
     </BrowserRouter>
   );
 };
