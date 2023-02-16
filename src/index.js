61aba850c43584bf2cb85ff4237040edef2f3853
Olena <newsroommy@gmail.com>
2023-02-16 09:52:33 +0300

    add to index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { App } from 'App';
 import css from './shared/styles/styles.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter className={css.styles}>
+    <BrowserRouter className={css.styles} basename="/goit-react-hw-05-movies">
       <App />
     </BrowserRouter>
   </React.StrictMode>
